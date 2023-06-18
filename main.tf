c1de3dbaf2b1d41058189791212c00aeac57f85f
Bryan Overbey <bryanmorganoverbey@gmail.com>
2023-06-18 13:08:31 -0400

    load balancer" git push

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ module "blog_vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -36,6 +35,54 @@ resource "aws_instance" "blog" {
     Name = "Learning Terraform"
   }
 }
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.this_security_group_id
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+
+      }
+    }
+  ]
+
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
