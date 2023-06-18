d220eb817c26c5d8a4dae2186a12be0b0d51cc22
Bryan Overbey <bryanmorganoverbey@gmail.com>
2023-06-18 12:19:57 -0400

    use vpc+id

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.this_security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
@@ -44,7 +44,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id              = module.blog_vpc.public_subnets[0]
+  vpc_id              = module.blog_vpc.vpc_id
   name                = "blog"
   ingress_rules       = ["https-443-tcp", "http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
