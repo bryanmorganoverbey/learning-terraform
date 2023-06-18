12efff9922752f7e76fb996cb31833a93d0725fb
Bryan Overbey <bryanmorganoverbey@gmail.com>
2023-06-18 19:08:10 -0400

    autoscaling

--- main.tf
+++ main.tf
@@ -28,15 +28,24 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+  # insert the 1 required variable here
+  name             = "blog"
+  min_size         = 1
+  max_size         = 2
+  desired_capacity = 1
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  tags = {
-    Name = "Learning Terraform"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
